4020d64bdb3042d401c7def0d84cce7fb95a1094
StefanLjiljak <stefanljiljak@gmail.com>
2020-06-22 17:32:01 +0200

    Initial dist subtree commit

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 const audio = document.querySelector('#audio')
+const alertModal = document.querySelector('#alertModal')
+const cardModal = document.querySelector('#cardModal')
+const headbang = document.querySelector('#headbang')
+const influence = document.querySelector('#influence')
 
-document.addEventListener('DOMContentLoaded', audio.play())
+alertModal.addEventListener('click', (e) => {
+    console.log('play', e.target)
+    audio.play()
+})
+
+cardModal.addEventListener('click', (e) => {
+    console.log('play', e.target)
+    audio.play()
+})
+
+headbang.addEventListener('click', (e) => {
+    console.log('play', e.target)
+    audio.play()
+})
+
+influence.addEventListener('click', (e) => {
+    console.log('play', e.target)
+    audio.play()
+})
+
